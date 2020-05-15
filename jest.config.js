f4afb7c8dbd78ec62eb14e6de98ba7a6db8fe4db
Charlie Laabs <charlielaabs@gmail.com>
2020-05-15 10:37:16 -0500

    Update Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: [
+    "**/test/**/*.test.ts"
+  ],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/dist/"
+  ]
 };
