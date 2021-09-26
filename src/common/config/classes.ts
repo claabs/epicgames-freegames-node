61f4acae9aaab1d4e21280cca730be0d03ecb113
Charlie Laabs <charlielaabs@gmail.com>
2021-09-26 15:47:45 -0500

    Populate email and web server environment variables. Add @env tags.

--- src/common/config/classes.ts
+++ src/common/config/classes.ts
@@ -27,14 +27,16 @@ import { ListenOptions } from 'net';
 export class EmailAuthConfig {
   /**
    * The SMTP username (if necessary)
-   * @example "hello@gmail.com"
+   * @example hello@gmail.com
+   * @env SMTP_USERNAME
    */
   @IsString()
   user: string;
 
   /**
    * The SMTP password (if necessary)
-   * @example "abc123"
+   * @example abc123
+   * @env SMTP_PASSWORD
    */
   @IsString()
   pass: string;
@@ -43,7 +45,8 @@ export class EmailAuthConfig {
 export class EmailConfig {
   /**
    * The outgoing SMTP host name
-   * @example "smtp.gmail.com"
+   * @example smtp.gmail.com
+   * @env SMTP_HOST
    */
   @IsUrl()
   smtpHost: string;
@@ -51,6 +54,7 @@ export class EmailConfig {
   /**
    * The outgoing SMTP port (SSL or TLS, see secure)
    * @example 587
+   * @env SMTP_PORT
    */
   @IsInt()
   @Min(0)
@@ -59,21 +63,24 @@ export class EmailConfig {
 
   /**
    * The sender of the email you will recieve (can be your email address)
-   * @example "hello@gmail.com"
+   * @example hello@gmail.com
+   * @env EMAIL_SENDER_ADDRESS
    */
   @IsEmail()
   emailSenderAddress: string;
 
   /**
    * The name of the email sender
-   * @example "Epic Games Captchas"
+   * @example Epic Games Captchas
+   * @env EMAIL_SENDER_NAME
    */
   @IsString()
   emailSenderName: string;
 
   /**
    * The recipient of the email (can be your email address)
-   * @example "hello@gmail.com"
+   * @example hello@gmail.com
+   * @env EMAIL_RECIPIENT_ADDRESS
    */
   @IsEmail()
   emailRecipientAddress: string;
@@ -82,6 +89,7 @@ export class EmailConfig {
    * true for SSL (port 465), false for TLS or unsecure
    * @example true
    * @default false
+   * @env SMTP_SECURE
    */
   @IsBoolean()
   secure = false;
@@ -113,18 +121,20 @@ export class NotificationConfig {
 export class WebPortalConfig {
   /**
    * The URL base that will be returned when a captcha must be remotely solved
-   * @example "https://epic.example.com"
-   * @default "http://localhost:3000"
+   * @example https://epic.example.com
+   * @default http://localhost:3000
+   * @env BASE_URL
    */
   @IsOptional()
   @IsUrl({
     require_tld: false,
   })
-  baseUrl = process.env.BASE_URL;
+  baseUrl?: string;
 
   /**
    * Node Net.listen options: https://nodejs.org/api/net.html#net_server_listen_options_callback
    * @default { port: 3000 }
+   * @env SERVER_PORT (for `{port: SERVER_PORT}` only)
    */
   @IsOptional()
   @IsObject()
@@ -141,14 +151,16 @@ export class WebPortalConfig {
 export class AccountConfig {
   /**
    * Epic Games login email
-   * @example "example@gmail.com"
+   * @example example@gmail.com
+   * @env EMAIL
    */
   @IsEmail()
   email: string;
 
   /**
    * Epic Games login password
-   * @example "abc123"
+   * @example abc1234
+   * @env PASSWORD
    */
   @IsString()
   @MinLength(7)
@@ -156,7 +168,8 @@ export class AccountConfig {
 
   /**
    * If 2FA is enabled, add your TOTP secret
-   * @example "EMNCF83ULU3K3PXPJBSWY3DPEHPK3PXPJWY3DPEHPK3YI69R39NE"
+   * @example EMNCF83ULU3K3PXPJBSWY3DPEHPK3PXPJWY3DPEHPK3YI69R39NE
+   * @env TOTP
    */
   @IsOptional()
   @Length(52)
@@ -190,8 +203,9 @@ export enum LogLevel {
 export class Config {
   /**
    * Cron string of when to run the process. If using TZ=UTC, a value of 5 16 * * * will run 5 minutes after the new games are available
-   * @example "5 16 * * *"
-   * @default "0 12 * * *"
+   * @example 5 16 * * *
+   * @default 0 12 * * *
+   * @env CRON_SCHEDULE
    */
   @IsOptional()
   @IsString()
@@ -199,8 +213,9 @@ export class Config {
 
   /**
    * The search criteria for finding free games. Either the weekly promotion, and free promotion, or all free products.
-   * @example "promotion"
-   * @default "weekly"
+   * @example promotion
+   * @default weekly
+   * @env SEARCH_STRATEGY
    */
   @IsOptional()
   @IsEnum(SearchStrategy)
@@ -210,6 +225,7 @@ export class Config {
    * If true, the process will run on startup in addition to the scheduled time.
    * @example true
    * @default false
+   * @env RUN_ON_STARTUP
    */
   @IsOptional()
   @IsBoolean()
@@ -227,8 +243,9 @@ export class Config {
 
   /**
    * Log level in lower case. Can be [silent, error, warn, info, debug, trace]
-   * @example "debug"
-   * @default "info"
+   * @example debug
+   * @default info
+   * @env LOG_LEVEL
    */
   @IsOptional()
   @IsEnum(LogLevel)
@@ -236,7 +253,8 @@ export class Config {
 
   /**
    * A unique hCaptcha accessibility URL recieved in your email after signing up here: https://dashboard.hcaptcha.com/signup?type=accessibility
-   * @example "https://accounts.hcaptcha.com/verify_email/96e9d77b-21eb-463d-9a21-75237fb27b6c"
+   * @example https://accounts.hcaptcha.com/verify_email/96e9d77b-21eb-463d-9a21-75237fb27b6c
+   * @env HCAPTCHA_ACCESSIBILITY_URL
    */
   @IsOptional()
   @IsUrl()
@@ -257,6 +275,7 @@ export class Config {
    * Default to purchasing games using browser automation
    * @example true
    * @default false
+   * @env PUPPETEER_PURCHASE
    */
   @IsOptional()
   @IsBoolean()
@@ -298,6 +317,7 @@ export class Config {
 
   /**
    * Deprecated, use `searchStrategy`
+   * @env ONLY_WEEKLY
    */
   @IsOptional()
   @IsBoolean()
@@ -314,6 +334,61 @@ export class Config {
       account.password = PASSWORD;
       account.totp = TOTP;
       this.accounts = [account];
+    }
+
+    // Use environment variables to fill email notification config if present
+    const {
+      SMTP_HOST,
+      SMTP_PORT,
+      EMAIL_SENDER_ADDRESS,
+      EMAIL_SENDER_NAME,
+      EMAIL_RECIPIENT_ADDRESS,
+      SMTP_SECURE,
+      SMTP_USERNAME,
+      SMTP_PASSWORD,
+    } = process.env;
+    if (
+      SMTP_HOST &&
+      SMTP_PORT &&
+      EMAIL_SENDER_ADDRESS &&
+      EMAIL_SENDER_NAME &&
+      EMAIL_RECIPIENT_ADDRESS &&
+      SMTP_SECURE
+    ) {
+      const email = new EmailConfig();
+      email.smtpHost = SMTP_HOST;
+      email.smtpPort = parseInt(SMTP_PORT, 10);
+      email.emailSenderAddress = EMAIL_SENDER_ADDRESS;
+      email.emailSenderName = EMAIL_SENDER_NAME;
+      email.emailRecipientAddress = EMAIL_RECIPIENT_ADDRESS;
+      email.secure = SMTP_SECURE === 'true';
+      if (SMTP_USERNAME && SMTP_PASSWORD) {
+        const auth = new EmailAuthConfig();
+        auth.user = SMTP_USERNAME;
+        auth.pass = SMTP_PASSWORD;
+        email.auth = auth;
+      }
+      if (!this.notification) {
+        this.notification = new NotificationConfig();
+      }
+      this.notification.email = email;
+    }
+
+    // Use environment variables to fill webPortalConfig if present
+    const { BASE_URL, SERVER_PORT } = process.env;
+    if (BASE_URL) {
+      if (!this.webPortalConfig) {
+        this.webPortalConfig = new WebPortalConfig();
+      }
+      this.webPortalConfig.baseUrl = BASE_URL;
+    }
+    if (SERVER_PORT) {
+      if (!this.webPortalConfig) {
+        this.webPortalConfig = new WebPortalConfig();
+      }
+      this.webPortalConfig.listenOpts = {
+        port: parseInt(SERVER_PORT, 10),
+      };
     }
   }
 }
