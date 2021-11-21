ea76dc2a3c4a95a70849ab9ea89b0b07d44227ef
Charlie Laabs <charlielaabs@gmail.com>
2021-11-21 16:57:12 -0600

    More logging in version check

--- src/entrypoint-config.ts
+++ src/entrypoint-config.ts
@@ -9,8 +9,13 @@ const PROJECT_NAME = 'epicgames-freegames-node';
 async function checkForUpdate(): Promise<void> {
   const { COMMIT_SHA, BRANCH } = process.env;
   if (!(COMMIT_SHA && BRANCH) || config.skipVersionCheck) {
+    L.debug(
+      { COMMIT_SHA, BRANCH, skipVersionCheck: config.skipVersionCheck },
+      'Skipping version check'
+    );
     return;
   }
+  L.debug({ PROJECT_NAME, BRANCH, COMMIT_SHA }, 'Performing version check');
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const resp = await got.get<any>(
     `https://api.github.com/repos/claabs/${PROJECT_NAME}/commits/${BRANCH}`,
@@ -19,6 +24,7 @@ async function checkForUpdate(): Promise<void> {
     }
   );
   const latestSha = resp.body.sha;
+  L.trace({ latestSha }, 'Response from GitHub API');
   if (COMMIT_SHA !== latestSha) {
     L.warn(
       `A newer version of ${PROJECT_NAME} is available! \`docker pull\` this image to update.`
