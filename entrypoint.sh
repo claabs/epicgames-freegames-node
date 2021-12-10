6a08a53b6ab72b4db14821c001a322b6c005158c
Charlie Laabs <charlielaabs@gmail.com>
2021-12-10 11:07:00 -0600

    Ensure single instance on Debian image

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,8 +33,8 @@ if [ "$RUN_ONCE" = "false" ]; then
     else
         # Debian cron wipes the environment, so we save it to a script to load in cron
         printenv | sed 's/^\(.*\)$/export \1/g' > /root/project_env.sh
-        # Add the command to the crontab
-        echo "${CRON_SCHEDULE} . /root/project_env.sh && cd /usr/app && node /usr/app/dist/src/index.js > /proc/1/fd/1 2>/proc/1/fd/2" | crontab -
+        # Add the command to the crontab. Debian cron doesn't ensure single instance, so we use flock to ensure it
+        echo "${CRON_SCHEDULE} . /root/project_env.sh && cd /usr/app && flock -n /var/lock/epicgames.lock node /usr/app/dist/src/index.js > /proc/1/fd/1 2>/proc/1/fd/2" | crontab -
         # Run the cron process. The container should halt here and wait for the schedule.
         cron -f
     fi
