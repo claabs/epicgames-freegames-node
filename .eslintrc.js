7ff46627c30350c6cb34578ce35563e08aa244c3
Charlie Laabs <charlielaabs@gmail.com>
2022-05-08 19:50:58 -0500

    Simplify eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,29 +8,19 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
     'plugin:jest/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
     sourceType: 'module'
   },
-  plugins: ['@typescript-eslint', 'jest'],
   rules: {
     'import/extensions': 0,
     'import/prefer-default-export': 0,
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': ['error'],
-  },
-  settings: {
-    'import/extensions': ['.js', '.ts',],
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts']
-    },
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.ts',]
-      }
-    }
   },
   ignorePatterns: ['dist/**', 'node_modules/**', '**/*.config.js', '.eslintrc.js', 'docs/**'],
 }
