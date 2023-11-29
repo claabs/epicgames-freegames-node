c932e702d6e071b60e9fd1e8ff8d973dcf67ff7e
charlocharlie <charlielaabs@gmail.com>
2023-11-29 09:41:16 -0600

    Fix localtunnel path mapping

--- src/common/localtunnel.ts
+++ src/common/localtunnel.ts
@@ -5,8 +5,6 @@ export async function getLocaltunnelUrl(originalUrl: string): Promise<string> {
   const url = new URL(originalUrl);
   const tunnel = await localtunnel(config.webPortalConfig?.listenOpts?.port || 3000);
   const tunnelUrl = new URL(tunnel.url);
-  Array.from(url.searchParams.entries()).forEach(([name, val]) => {
-    tunnelUrl.searchParams.set(name, val);
-  });
+  tunnelUrl.pathname = url.pathname;
   return tunnelUrl.toString();
 }
